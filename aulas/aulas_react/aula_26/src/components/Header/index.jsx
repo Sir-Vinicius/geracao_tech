a575635434263227eeab79999f2a018c2fbf143d
Sir-Vinicius <viniciusidai@gmail.com>
2024-10-16 16:50:22 -0300

    Att aulas

--- aulas/aulas_react/aula_26/src/components/Header/index.jsx
+++ aulas/aulas_react/aula_26/src/components/Header/index.jsx
@@ -3,13 +3,21 @@ import styles from './Header.module.css'
 export function Header() {
     return (
         <header className={styles.header}>
-            <span>Loja</span>
+            <div className={styles.headerTop}>
+            <span>Icone</span>
             <nav>
-                <a href="#">Home</a>
-                <a href="#">Sobre</a>
-                <a href="#">Produtos</a>
-                <a href="#">Contatos</a>
+                <a href="#">Busca</a>
+                <a href="#">Link Cad</a>
+                <a href="#">Entrar</a>
+                <a href="#">Carrinho</a>
             </nav>
+            </div>
+            <div className={styles.headerBottom}>
+                <a href="">Home</a>
+                <a href="">Sobre</a>
+                <a href="">Produtos</a>
+                <a href="">Contato</a>
+            </div>
         </header>
     );
 }
